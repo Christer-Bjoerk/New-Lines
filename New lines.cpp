810b5a0c81faa5e6beeb64fc47dc58dd96f705ac
Christer Bjoerk <productivity.christerbjoerk@gmail.com>
2023-02-02 11:36:35 +0100

    First commit
    How to use new lines command

---
+++ New lines.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	// Create a new line with \n character
+	cout << "Hello World \n";
+
+	// Two \n creates a blank space
+	cout << "Hello World \n\n";
+	cout << "I'm learning C++ \n\n";
+
+	// endl is an alternative to \n
+	cout << "Hello World" << endl;
+	cout << "I'm learning C++";
+
+	return 0;
+}
