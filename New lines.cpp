996b9a3ec16258696daa9115d84bc4ac5fd5665b
Christer Bjoerk <productivity.christerbjoerk@gmail.com>
2023-02-02 11:43:27 +0100

    Adding extra content

--- New lines.cpp
+++ New lines.cpp
@@ -13,7 +13,15 @@ int main()
 
 	// endl is an alternative to \n
 	cout << "Hello World" << endl;
-	cout << "I'm learning C++";
+	cout << "I'm learning C++\n" << endl;
 
+	// Horizontal tab sequence
+	cout << "This is me using\t the horizontal tab\n" << endl;
+
+	// Backslash character
+	cout << "This is how to create a backlash character\\ \n\n";
+
+	// Writing a Quote
+	cout << "\"Quote\".";
 	return 0;
 }
